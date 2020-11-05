6e5ab1ff197ff7d02d4ae3155c97afa4209d98b5
yuzden <yuzden.4729@gmail.com>
2020-11-05 14:45:47 +0200

    exercise2

---
+++ Arrays/exc4.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int n = 0;
+	int number = 0;
+	cin >> n;
+
+	int max_value = INT16_MIN;
+	int min_value = INT16_MAX;
+	
+	for (int i = 0; i < n; i++) {
+		cin >> number; 
+
+		if (number > max_value) {
+			max_value = number;
+		}
+		if (number < min_value) {
+			min_value = number;
+		}
+	}
+	cout << max_value << " " << min_value;
+
+	return 0;
+}
